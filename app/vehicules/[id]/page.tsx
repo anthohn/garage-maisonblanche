21b89dbd8e54c27c257e63bd100915440a2f6408
antho <anthonyhohn@gmail.com>
2026-01-17 15:24:52 +0100

    correction erreur next

--- app/vehicules/[id]/page.tsx
+++ app/vehicules/[id]/page.tsx
@@ -81,7 +81,7 @@ export default function CarDetailPage({ params }: { params: Promise<{ id: string
                             </h1>
 
                             <div className="text-3xl font-bold text-[#303F9F] mb-6">
-                                {car.price.toLocaleString()} CHF
+                                {car.price.toLocaleString('fr-CH').replace(/\s/g, "'")}&nbsp;CHF
                             </div>
 
                             {/* Key Information */}
@@ -118,7 +118,7 @@ export default function CarDetailPage({ params }: { params: Promise<{ id: string
                                             <path d="M61.44,0A61.46,61.46,0,1,1,18,18,61.23,61.23,0,0,1,61.44,0Zm4.07,82.09a6.67,6.67,0,1,1-8.14,0V68.62H42.31V82.09a6.67,6.67,0,1,1-8.14,0V46.17a6.67,6.67,0,1,1,8.14,0V60.48H57.37V46.17a6.67,6.67,0,1,1,8.14,0V60.48H80.57V46.17a6.67,6.67,0,1,1,8.14,0V64a4.41,4.41,0,0,1,0,.52,4.07,4.07,0,0,1-4.07,4.07H65.51V82.09Zm33-57.76a52.46,52.46,0,1,0,15.38,37.11A52.29,52.29,0,0,0,98.55,24.33Z" />
                                         </svg>
                                     </div>
-                                    <div className="text-sm text-gray-600">Transmision</div>
+                                    <div className="text-sm text-gray-600">Transmission</div>
                                     <div className="font-semibold text-gray-900">{car.transmission}</div>
                                 </div>
                             </div>
