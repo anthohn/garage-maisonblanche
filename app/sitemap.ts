660239dbf91c928bd9a18ff7f3b7b170cf8773fb
Anthony Höhn <anthonyhohn@gmail.com>
2026-06-10 14:19:35 +0200

    feat : ajout mentions-légales sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -27,6 +27,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'yearly',
             priority: 0.7,
         },
+        {
+            url: 'https://garage-maisonblanche.ch/mentions-legales',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
     ];
 
     const vehicleRoutes: MetadataRoute.Sitemap = carsData.map((car) => ({
